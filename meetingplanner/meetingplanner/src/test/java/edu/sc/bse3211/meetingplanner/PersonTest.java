62e220648e12a2049b6324a24d2218c5367923b1
Jim_isaq <jimisaac8082@gmail.com>
2026-05-03 14:59:08 +0300

    Added tests for checking room and person availability

--- meetingplanner/meetingplanner/src/test/java/edu/sc/bse3211/meetingplanner/PersonTest.java
+++ meetingplanner/meetingplanner/src/test/java/edu/sc/bse3211/meetingplanner/PersonTest.java
@@ -3,7 +3,74 @@ package edu.sc.bse3211.meetingplanner;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class PersonTest {
-	// Add test methods here. 
-    // You are not required to write tests for all classes.
+	// Checking availabilty of a person
+    // should not be busy when no meeting exists for them
+    @Test
+    public void personShouldNotBeBusyWhenNoMeetingExists() throws TimeConflictException {
+        Person person = new Person("Ainamani Jim");
+        boolean result = person.isBusy(5, 4, 9, 10);
+
+        assertFalse("The person should not be busy when no meeting exists.", result);
+    }
+
+    //should be busy when a meeting exists for them
+    @Test
+    public void personShouldBeBusyWhenMeetingAlreadyExists() throws TimeConflictException {
+        Person person = new Person("Ainamani Jim");
+        Room room = new Room("CR1");
+
+        ArrayList<Person> attendees = new ArrayList<Person>();
+        attendees.add(person);
+
+        Meeting meeting = new Meeting(5, 4, 9, 10, attendees, room, "Staff Meeting");
+
+        person.addMeeting(meeting);
+        boolean result = person.isBusy(5, 4, 9, 10);
+
+        assertTrue("The person should be busy when a meeting already exists.", result);
+    }
+
+    // Checking a person with an invalid day (should throw and exception)
+    @Test
+    public void checkingPersonWithInvalidDayShouldThrowException() {
+        Person person = new Person("Ainamani Jim");
+
+        try {
+            person.isBusy(2, 35, 9, 10);
+            fail("Expected TimeConflictException because day 35 does not exist.");
+        } catch (TimeConflictException e) {
+            assertEquals("Day does not exist.", e.getMessage());
+        }
+    }
+
+    //Checking a person with an invalid hour (should throw an exception)
+    @Test
+    public void checkingPersonWithInvalidHourShouldThrowException() {
+        Person person = new Person("Ainamani Jim");
+
+        try {
+            person.isBusy(5, 4, -1, 10);
+
+            fail("Expected TimeConflictException because -1 is an illegal hour.");
+        } catch (TimeConflictException e) {
+            assertEquals("Illegal hour.", e.getMessage());
+        }
+    }
+
+    //Checking a person where start time is after end time (should throw an exception)
+    @Test
+    public void checkingPersonWhereStartTimeIsAfterEndTimeShouldThrowException() {
+        Person person = new Person("Ainamani Jim");
+
+        try {
+            person.isBusy(5, 4, 15, 10);
+
+            fail("Expected TimeConflictException because start time is after end time.");
+        } catch (TimeConflictException e) {
+            assertEquals("Meeting starts before it ends.", e.getMessage());
+        }
+    }
 }
--- meetingplanner/meetingplanner/src/test/java/edu/sc/bse3211/meetingplanner/RoomTest.java
+++ meetingplanner/meetingplanner/src/test/java/edu/sc/bse3211/meetingplanner/RoomTest.java
@@ -3,7 +3,78 @@ package edu.sc.bse3211.meetingplanner;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class RoomTest {
 	// Add test methods here. 
     // You are not required to write tests for all classes.
+
+    //Checking availability of a room
+
+    //A room should not be busy when no meeting exists
+    @Test
+    public void roomShouldNotBeBusyWhenNoMeetingExists() throws TimeConflictException {
+        Room room = new Room("CR1");
+        boolean result = room.isBusy(5,4,9,10);
+        assertFalse("The room should not be busy when no meeting exists.",result);
+    }
+
+    //A room should be busy when a meeting already exists
+    @Test
+    public void roomShouldBeBusyWhenMeetingAlreadyExists() throws TimeConflictException {
+        Room room = new Room("CR1");
+        Person person = new Person("Ainamani Jim");
+
+        ArrayList<Person> attendees = new ArrayList<Person>();
+        attendees.add(person);
+
+        Meeting meeting = new Meeting(5, 4, 9, 10, attendees, room, "Board Meeting");
+
+        room.addMeeting(meeting);
+
+        boolean result = room.isBusy(5, 4, 9, 10);
+        assertTrue("The room should be busy when a meeting already exists.", result);
+    }
+
+    //Checking a room with an invalid day (should throw an exception)
+    @Test
+    public void checkingRoomWithInvalidDayShouldThrowException() {
+        Room room = new Room("CR1");
+
+        try {
+            room.isBusy(2, 35, 9, 10);
+            fail("Expected TimeConflictException because day 35 does not exist.");
+        } catch (TimeConflictException e) {
+            assertEquals("Day does not exist.", e.getMessage());
+        }
+    }
+
+    //Checking a room with an invalid hour (should throw an exception)
+    @Test
+    public void checkingRoomWithInvalidHourShouldThrowException() {
+        Room room = new Room("CR1");
+
+        try {
+            room.isBusy(5, 4, -1, 10);
+            fail("Expected TimeConflictException because -1 is an illegal hour.");
+        } catch (TimeConflictException e) {
+            assertEquals("Illegal hour.", e.getMessage());
+        }
+    }
+
+    //Checking room where start time is after end time (should throw an exception)
+    @Test
+    public void checkingRoomWhereStartTimeIsAfterEndTimeShouldThrowException() {
+        Room room = new Room("CR1");
+
+        try {
+            room.isBusy(5, 4, 15, 10);
+
+            fail("Expected TimeConflictException because start time is after end time.");
+        } catch (TimeConflictException e) {
+            assertEquals("Meeting starts before it ends.", e.getMessage());
+        }
+    }
+
+
 }
